367893d81c1dedfc12a55f703ee385e38910e5f8
Nick-Kyriakopoulos <nick.kyriakopoulos123@gmail.com>
2025-08-28 10:37:22 +0300

    End of section 6

--- Carsties/src/IdentityService/Config.cs
+++ Carsties/src/IdentityService/Config.cs
@@ -28,7 +28,20 @@ public static class Config
                 RedirectUris = { "https://www.getpostman.com/oauth2/callback" },
                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword
-
+            },
+            
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "Next.js App",
+                ClientSecrets = { new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30
             }
+
         };
 }
