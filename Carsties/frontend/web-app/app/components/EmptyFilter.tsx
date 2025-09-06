f394033bffb113de7df2e5d8186925b70999c453
Nick-Kyriakopoulos <nick.kyriakopoulos123@gmail.com>
2025-09-06 12:36:00 +0300

    End of section 9

--- Carsties/frontend/web-app/app/components/EmptyFilter.tsx
+++ Carsties/frontend/web-app/app/components/EmptyFilter.tsx
@@ -1,17 +1,24 @@
+'use client';
+
 import { useParamsStore } from "@/hooks/useParamsStore";
 import Heading from "./Heading";
 import { Button } from "flowbite-react/components/Button";
+import { signIn } from "next-auth/react";
 
 type Props = {
     title?: string;
     subtitle?: string;
     showReset?: boolean;
+    showLogin?: boolean;
+    callbackUrl?: string;
 }
 
 export default function EmptyFilter({
     title= 'No matches for this filter',
     subtitle= 'Try changing your filter criteria or search term', 
-    showReset
+    showReset,
+    showLogin,
+    callbackUrl
     }: Props) {
         const reset = useParamsStore(state => state.reset);
 
@@ -21,6 +28,11 @@ export default function EmptyFilter({
                 <div className="mt-4">
                     {showReset && (
                         <Button onClick={reset}>Remove Filters</Button>
+                    )}
+                    {showLogin && (
+                        <Button onClick={() => signIn("id-server", { redirectTo: callbackUrl })}>
+                            Login
+                        </Button>
                     )}
                 </div>
             </div>
---
+++ Carsties/frontend/web-app/app/nav/LoginButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Button } from "flowbite-react";
+import { signIn } from "next-auth/react";
+
+export default function LoginButton() {
+  return (
+    <div>
+        <Button outline onClick={() => signIn('id-server', {redirectTo:'/' }, {prompt: 'login'})}>
+            Login
+        </Button>
+    </div>
+  )
+}
